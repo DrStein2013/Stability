192266ef7f59c763d263bab49c49854955dd1cba
DrStein2013 <mirror96@gmail.com>
2016-01-13 13:43:16 +0400

    Удален

--- Stability/Model/DeviceConfig.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Stability.Model
-{
-    public class CPortConfig
-    {
-        public string PortName { get; set; }
-        public int Baud { get; set; }
-        public bool AutoConnect { get; set; }
-    }
-}
