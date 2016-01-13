1b9429554064c5a4a55d2404d77a679a7c888697
DrStein2013 <mirror96@gmail.com>
2016-01-13 13:33:31 +0400

    Добавил событие изменения статуса порта, а также класс аргументов для него

--- Stability/Model/Port/IPort.cs
+++ Stability/Model/Port/IPort.cs
@@ -2,12 +2,37 @@
 
 namespace Stability.Model.Port
 {
+    /// <summary>
+    /// Аргумент для события изменения статуса прота
+    /// </summary>
+    public class PortStatusChangedEventArgs : EventArgs
+    {
+        public EPortStatus Status { get; set; }
+    }
+
+    /// <summary>
+    /// Интерфейс, описывающий основные функции портов, используется для абстрагирования от конкретного порта
+    /// </summary>
     public interface IPort
     {
-        event EventHandler rx_event;
-
+        /// <summary>
+        /// Событие вычитки из порта, взводится, когда есть что читать.
+        /// Привязать на это событие обработчик вычитки данных
+        /// </summary>
+        /// <remarks></remarks>
+        event EventHandler RxEvent;
+        /// <summary>
+        /// Событие изменения статуса порта, показывает открыт порт, или нет
+        /// </summary>
+        event EventHandler<PortStatusChangedEventArgs> PortStatusChanged;
+        /// <summary>
+        /// Метод открытия порта
+        /// </summary>
         bool Connect(out string msg);
 
+        /// <summary>
+        /// Метод закрытия порта
+        /// </summary>
         bool Disconnect();
     }
 }
