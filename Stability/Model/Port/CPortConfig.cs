dc02d803ac6cc4ca8b1debe50cc6bbeeaa03db40
DrStein2013 <mirror96@gmail.com>
2016-01-13 13:38:30 +0400

    Структура для первоначальной настройки порта

---
+++ Stability/Model/Port/CPortConfig.cs
@@ -0,0 +1,9 @@
+﻿namespace Stability.Model.Port
+{
+    public class CPortConfig
+    {
+        public string PortName { get; set; }
+        public int Baud { get; set; }
+        public bool AutoConnect { get; set; }
+    }
+}
