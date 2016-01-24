c59af19b29333b6caa9f12b658d02e71d79564a7
DrStein2013 <mirror96@gmail.com>
2016-01-24 21:34:06 +0300

    Разрабатываю интерфейс модели и бизнес логику приложения

--- Stability/Model/StabilityModel.cs
+++ Stability/Model/StabilityModel.cs
@@ -4,31 +4,59 @@ using System.Linq;
 using System.Text;
 using Stability.Model.Device;
 using Stability.Model.Port;
+using System.Threading;
+using Stability.View;
+
 
 namespace Stability.Model
 {
+
+    public class TenzEventArgs : EventArgs
+    {
+        public double[] Data { get; set; }
+    }
+
     interface IStabilityModel
     {
-        event EventHandler UpdateDataView;
+        event EventHandler<TenzEventArgs> UpdateDataView;
+        void DeviceCmdFromView(DeviceCmd c);
     }
 
     public class StabilityModel : IStabilityModel
     {
         private StabilityDevice _device;
-        public event EventHandler UpdateDataView;
+        public event EventHandler<TenzEventArgs> UpdateDataView;
+
+        private readonly Timer _viewUpdaterTimer;
 
         public StabilityModel()
         {
             var conf = MainConfig.PortConfig;
             IoC.GetKernel().Bind<IPort>().To<CComPort>().InSingletonScope().WithConstructorArgument("config", conf);
             _device = new StabilityDevice();
+            
+            _viewUpdaterTimer = new Timer(ViewTimerHandler, null,100, 60);
         }
 
-        public void SetPortHandler(EventHandler<PortStatusChangedEventArgs> pHandler)
+        private void ViewTimerHandler(object state)
         {
-            IoC.Resolve<IPort>().PortStatusChanged += pHandler;
+            if (UpdateDataView != null) 
+                UpdateDataView(this, new TenzEventArgs() {Data = _device.CurrAdcVals});
         }
 
-
+        public void DeviceCmdFromView(DeviceCmd c)
+        {
+            switch (c)
+            {
+                    case DeviceCmd.START_MEASURE:
+                        _device.StartMeasurement();
+                    break;
+                    case DeviceCmd.STOP_MEASURE:
+                        _device.StopMeasurement();
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
